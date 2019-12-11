42a762df4f7ef5a59f5a3c03a94eb97edac835f3
CiDavidCo <daveeaster47@hotmail.com>
2019-12-11 12:37:30 +0100

    Added reveal.js

---
+++ reveal.js/js/script.js
@@ -0,0 +1,51 @@
+/* Event Listeners: Fragments */
+var oshown = Array(15).fill(false);
+var lshown = Array(20).fill(false);
+var lshown = Array(10).fill(false);
+var dshown = Array(10).fill(false);
+var ashown = Array(10).fill(false);
+
+Reveal.addEventListener('fragmentshown', function (event) {
+
+
+    /*
+
+        OVIDIU
+
+    if (event.fragment.id === 'o-1' && !oshown[0]) {
+        setTimeout(function () {
+            consoleText(['program'], 'o-program', ['#76ff03'], 500, 'program');
+            oshown[0] = true;
+        }, 500);
+    }
+    */
+
+
+    /*
+
+        LUCIANO
+        
+    if (event.fragment.id === 'l-1' && !lshown[0]) {
+        
+    }
+
+    /*
+
+        DAVID
+
+    if (event.fragment.id === 'd-2' && !dshown[1]) {
+
+    }
+    */
+
+
+
+    /*
+
+        ANGELO
+
+    if (event.fragment.id === 'a-1' && !ashown[0]) {
+
+    }*/
+
+});
